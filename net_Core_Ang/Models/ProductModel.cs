a6930cb8fab86c0fa10c595e10b440ee0325e1d1
Michael <mikoa04@gmail.com>
2020-04-16 21:47:25 -0400

    Update ProductModel.cs
    Created the Product Model

--- net_Core_Ang/Models/ProductModel.cs
+++ net_Core_Ang/Models/ProductModel.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace net_Core_Ang.Models
 {
     public class ProductModel
     {
-        public ProductModel()
-        {
-        }
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string Description { get; set; }
+        [Required]
+        public bool OutOfStock { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        [Required]
+        public double Price{ get; set; }
+
     }
 }
